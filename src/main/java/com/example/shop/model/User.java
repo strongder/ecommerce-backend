85d667e65429e78b32e01147ad93b2569f352825
hieu <hieu.hua@ntq-solution.com.vn>
2025-01-06 13:46:52 +0700

    fix

--- src/main/java/com/example/shop/model/User.java
+++ src/main/java/com/example/shop/model/User.java
@@ -65,7 +65,7 @@ public class User {
 
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
     @PreUpdate
